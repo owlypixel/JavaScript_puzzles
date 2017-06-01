9b160e2950f42509de9aa430b2f17a107c94e738
Owlypixel <owlypixel@gmail.com>
2017-06-01 22:52:34 +0300

    create my owl es6 module

--- js_modules/app.js
+++ js_modules/app.js
@@ -1,7 +1,8 @@
 import { uniq } from 'lodash';
 import jsonp from 'jsonp';
-// import apiKey from './config';
-// console.log(apiKey);
+import { apiKey, url, sayHi } from './my_modules/config';
+console.log(apiKey, url);
+console.log(sayHi('owlypixel'));
 
 const ages = [1,5,43,52];
 
---
+++ js_modules/my_modules/config.js
@@ -0,0 +1,10 @@
+// named export
+export const apiKey = "abc123";
+export const url = "http://owlypixel.com";
+
+export function sayHi(name){
+	console.log(`Hello there ${name}`);
+}
+
+// default export
+// export default apiKey;
