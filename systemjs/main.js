6cf3bf651824e7e33b9d54d2b8a9fb9a1d2ef970
Owlypixel <owlypixel@gmail.com>
2017-06-02 22:36:57 +0300

    system JS

---
+++ systemjs/main.js
@@ -0,0 +1,5 @@
+console.log('Hello!');
+import {sum, kebabCase} from 'npm:lodash';
+import { addTax } from './checkout.js';
+console.log(kebabCase('Owlypixel is the new Owlytwister'));
+console.log(addTax(12, 45));
